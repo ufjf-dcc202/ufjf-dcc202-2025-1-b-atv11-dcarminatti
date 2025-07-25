1682a76d4c5de3c405ddbac899d1516c35e623f8
dcarminatti <dctti.amarante@gmail.com>
2025-07-24 21:21:12 -0300

    feat: criado funcao de render da lista de itens

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
+import { getLista, adicionaNaLista, removeDaLista, limpaLista } from "./lista.js";
+
 const olItens = document.getElementById("itens");
 const pEntrada = document.getElementById("entrada");
 const btnAdicionar = document.getElementById("adicionar");
 const btnRemover = document.getElementById("remover");
 const btnLimpar = document.getElementById("limpar");
+
+function atualizarLista() {
+  olItens.innerHTML = "";
+  const lista = getLista();
+  lista.forEach((item, index) => {
+    const li = document.createElement("li");
+    li.textContent = item;
+    li.dataset.index = index;
+    olItens.appendChild(li);
+  });
+}
+
+atualizarLista();
