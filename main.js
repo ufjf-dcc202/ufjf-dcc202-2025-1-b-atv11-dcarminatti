771424f23ad541badcc3155f2b94a631d65da1a4
dcarminatti <dctti.amarante@gmail.com>
2025-07-24 21:18:57 -0300

    feat: criado arquivo main.js

---
+++ main.js
@@ -0,0 +1,5 @@
+const olItens = document.getElementById("itens");
+const pEntrada = document.getElementById("entrada");
+const btnAdicionar = document.getElementById("adicionar");
+const btnRemover = document.getElementById("remover");
+const btnLimpar = document.getElementById("limpar");
